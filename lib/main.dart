b902f35de3571f92cb2d0c578d6a8033b6a4ceb8
sameeranand12 <201951134@iiitvadodara.ac.in>
2021-05-22 01:23:00 +0530

    Checking rows

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ void main() {
         body: SafeArea(
           child: Row(
 
-            crossAxisAlignment: CrossAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             //mainAxisSize: MainAxisSize.min,
             //verticalDirection: VerticalDirection.up,
 
@@ -34,31 +34,31 @@ void main() {
               ),
               Container(
                 padding: EdgeInsets.fromLTRB(10, 40, 20, 10),
-                height: 100.0,
-                width: 150.0,
+                //height: 100.0,
+                width: 30.0,
                 color: Colors.lightBlueAccent,
                 child: Text('Container 1'),
               ),
-              SizedBox(
-                height: 20.0,
-              ),
+             // SizedBox(
+                //height: 20.0,
+             // ),
               Container(
                 padding: EdgeInsets.fromLTRB(10, 40, 20, 10),
-                height: 120.0,
-                width: 150.0,
+               // height: 120.0,
+                width: 30.0,
                 color: Colors.pinkAccent,
                 child: Text('Container 2'),
               ),
               Container(
                 padding: EdgeInsets.fromLTRB(10, 40, 20, 10),
-                height: 120.0,
-                width: 250.0,
+                //height: 120.0,
+                width: 30.0,
                 color: Colors.tealAccent,
                 child: Text('Container 3'),
               ),//fsdf
-              Container(
-                width: double.infinity,
-              )
+              //Container(
+               // width: double.infinity,
+              //)
             ],
           ),
         ),
